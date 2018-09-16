24ec2c3d4b0c723b63e1af3ddfe27f31df84f4c0
rockn1989 <gold_100@bk.ru>
2018-09-16 21:12:34 +0300

    create projetc

---
+++ app/js/main.js
@@ -0,0 +1,96 @@
+'use strict';
+
+$(function() {
+
+
+	/*______ Показывать форму на мобильных устройствах ______*/
+
+	var $mobileForm = $('.mobile-form');
+
+	$('.js__show-form').on('click', function (e) {
+		e.preventDefault();
+		$mobileForm.slideToggle('350').find('input').focus();
+	})
+
+
+	/*______ Маска формы ______*/
+
+	$('.js__input-phone').mask('+7 999 999-99-99', {clearIfNotMatch: true}).focus(function (e) {
+		if (!$(this).val()) {
+			$(this).val('+7 ');
+		}
+	});
+
+
+	/*______ Валидация формы ______*/
+
+	if($('form').is('.default-form')) {
+
+		$('.default-form').validate({
+			rules: {
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				email: "Обязательноe поле",
+			},
+		});
+	};
+
+
+	/*______ Открытие мобильного подменю ______*/
+
+	$('.js__menu-sublist-toggle').on('click', function (e) {
+		e.preventDefault();
+		var self = $(this),
+			blockParent = self.parent('a'),
+			siblingsList = blockParent.siblings('.menu-sublist');
+
+		if(blockParent.hasClass('open')) {
+			siblingsList.stop().slideUp('350', function () {
+				blockParent.removeClass('open');
+			});
+		} else {
+			siblingsList.stop().slideDown('350', function () {
+				blockParent.addClass('open');
+			});
+		}
+
+		self.toggleClass('open');
+	});
+
+
+	/*______ Открытие мобильного подменю в футере ______*/
+
+	$('[data-role="toggle-list"] i').on('click', function (e) {
+		e.preventDefault();
+		var self = $(this),
+			blockParent = self.parent('[data-role="toggle-list"]'),
+			siblingsList = blockParent.parent().find('.footer__list');
+
+		self.toggleClass('open');
+		siblingsList.stop().slideToggle('350');
+	});
+
+
+	/*______ Отключение UIKIT анимации для мобильных устройств ______*/
+
+	UIkit.on('beforeready.uk.dom', function () {
+		if (UIkit.$win.width() < 767 && $('html').hasClass('uk-touch')) {
+			UIkit.$('[data-uk-scrollspy]').removeAttr('data-uk-scrollspy');
+		};
+	});
+
+
+	/*______ Полифил для Object-fit ______*/
+	
+	objectFitImages();
+
+
+	/*______ Полифил для SVG ______*/
+
+	svg4everebody();
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,218 @@
+
+var gulp = require("gulp"),
+		twig = require("gulp-twig"),
+		sass = require("gulp-sass"),
+		autoprefixer = require("gulp-autoprefixer"),
+		plumber = require("gulp-plumber"),
+		cmq = require('gulp-combine-media-queries'),
+		gcmq = require('gulp-group-css-media-queries'),
+		rename    = require("gulp-rename"),
+		htmlreplace= require("gulp-html-replace"),
+		minify = require("gulp-csso"),
+		imagemin  = require("gulp-imagemin"),
+		copy  = require("gulp-copy"),
+		del   = require("del"),
+		run = require("run-sequence"),
+		browserSync = require("browser-sync"),
+		svgstore  = require("gulp-svgstore"),
+		svgmin    = require("gulp-svgmin"),
+		svgSprite = require('gulp-svg-sprite'),
+		cheerio   = require("gulp-cheerio"),
+		clean     = require("gulp-clean");
+
+
+/*______ Templates ______*/
+
+gulp.task("templates", function() {
+  return gulp.src("app/templates/*.html")
+         .pipe(twig())
+         .pipe(gulp.dest('app/'))
+});
+
+
+/*______ Template Start ______*/
+
+gulp.task('dev', function() {
+  gulp.watch("app/templates/**/*.html", ["templates"], browserSync.reload);
+});
+
+
+/*______ Clean SVG-sprite ______*/
+
+gulp.task("clean", function() {
+	return gulp.src('app/img/icon-svg/sprite-svg.svg')
+					.pipe(clean({force: false}));
+});
+
+
+/*______ sass ______*/
+
+gulp.task("sass", function() {
+	return gulp.src("app/sass/main.sass")
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(autoprefixer(["last 15 version", "> 1%", 'firefox 14', "ie 8", "ie 7"], {cascade: true}))
+		.pipe(gcmq())
+		.pipe(rename("style.css"))
+		.pipe(gulp.dest("app/css"))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+
+/*______ sass production ______*/
+
+gulp.task("sass:build", function() {
+	return gulp.src("app/sass/main.sass")
+				.pipe(sass())
+				.pipe(autoprefixer(["last 15 version", "> 1%", 'firefox 14', "ie 8", "ie 7"], {cascade: true}))
+				.pipe(gulp.dest("app/css"))
+				.pipe(rename("style.css"))
+				.pipe(gulp.dest("build/app/css"));
+});
+
+
+/*______ SVG-sprite ______*/
+
+gulp.task("svgstoreDev",["clean"], function() {
+	return gulp.src("app/img/icon-svg/*.svg")
+				.pipe(svgstore({
+					inlineSvg: true
+				}))
+				.pipe(rename("sprite-svg.svg"))
+				.pipe(gulp.dest("app/img/icon-svg/"))
+});
+
+
+/*______ Local-server ______*/
+
+gulp.task("serve", ["templates", "sass", "svgstore"], function() {
+	browserSync.init({
+		server: "./app",
+		notify: false
+	});
+
+	gulp.watch("app/sass/**/*.sass", ["sass"]);
+	gulp.watch("app/img/icon-svg/*.svg", ["svgstoreDev"]);
+	gulp.watch("app/img/icon-svg/*.svg", browserSync.reload);
+	gulp.watch("app/js/**/*.js", browserSync.reload);
+	gulp.watch("app/*.html", browserSync.reload);
+
+});
+
+
+/*______ Minific Images ______*/
+
+gulp.task("images", function() {
+	return gulp.src("build/app/img/**/*.{png,jpg,gif}")
+		.pipe(imagemin([
+			imagemin.optipng({optimizationLevel: 3}),
+			imagemin.jpegtran({progressive: true})
+	]))
+	.pipe(gulp.dest("build/app/img"));
+});
+
+
+/*______ Concat and minific CSS and JS ______*/
+
+gulp.task("replace", function() {
+	gulp.src("app/*.html")
+	.pipe(htmlreplace({
+		"css": "css/min.style.css",
+		"js": "js/min.libs.js"
+	}))
+	.pipe(gulp.dest("build/app/"));
+});
+
+
+/*______ Copy Files ______*/
+
+gulp.task("copy", function() {
+	return gulp.src([
+		"app/fonts/**/*.{woff,woff2}",
+		"app/img/**",
+		"app/js/**",
+		"app/libs/**",
+		"app/*.html"
+	], {
+		base: "."
+	})
+	.pipe(gulp.dest("build"));
+});
+
+
+/*______ Del files ______*/
+
+gulp.task("del", function() {
+	return del("build");
+});
+
+
+/*______ Minific SVG ______*/
+
+gulp.task("svgmin", function() {
+	return gulp.src("app/img/icon-svg/*.svg")
+				.pipe(svgmin())
+				.pipe(gulp.dest("build/app/img/icon-svg"))
+});
+
+
+/*______ Remove attr Fill SVG ______*/
+
+gulp.task("removeSvgFill", function() {
+	return gulp.src("app/img/svg/*.svg")
+					.pipe(cheerio({
+						run: function($) {
+							$("[fill]").removeAttr("fill");
+						},
+						parseOptions: {xmlMode: true}
+					}))
+					.pipe(gulp.dest("app/img/svg/"))
+});
+
+
+/*______ Minific CSS ______*/
+
+gulp.task("cssmin", function() {
+	return gulp.src("build/app/css/style.css")
+				 .pipe(minify())
+				 .pipe(rename("style.min.css"))
+				 .pipe(gulp.dest("build/app/css"))
+});
+
+
+/*______ SVG sprite ______*/
+
+gulp.task("svgstore", function() {
+	gulp.src("app/img/icon-svg/*.svg")
+			.pipe(svgstore({
+				inlineSvg: true
+			}))
+			.pipe(rename("sprite-svg.svg"))
+			.pipe(gulp.dest("app/img/icon-svg/"));
+});
+
+
+/*______ SVG sprite production ______*/
+
+gulp.task("svgstore:build",["svgmin"], function() {
+	return gulp.src("build/app/img/icon-svg/*.svg")
+				.pipe(svgstore({
+					inlineSvg: true
+				}))
+				.pipe(rename("sprite-svg.svg"))
+				.pipe(gulp.dest("build/app/img/icon-svg"))
+});
+
+
+/*______ Build for production ______*/
+
+gulp.task("build", function() {
+	run(
+		"del",
+		"copy",
+		"sass:build",
+		"cssmin",
+		"svgstore:build",
+		"replace"
+	)
+});
