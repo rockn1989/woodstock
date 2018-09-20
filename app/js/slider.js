7c0fd8974d37992186c441c4ebec04086a4412b2
rockn1989 <gold_100@bk.ru>
2018-09-20 22:58:35 +0300

    detail desc

--- app/js/slider.js
+++ app/js/slider.js
@@ -39,6 +39,7 @@ $(function () {
 		centerMode: false,
 		lazyLoad: 'progressive',
 		focusOnSelect: true,
+		variableWidth: true,
 		responsive: [
 		{
 			breakpoint: 1245,
@@ -56,6 +57,34 @@ $(function () {
 			breakpoint: 767,
 			settings: {
 				slidesToShow: 2,
+			}
+		},
+	]
+	});
+
+
+	/*______ Products slider ______*/
+
+	 $('.products-slider .slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: true,
+		autoplay: false,
+		lazyLoad: 'progressive',
+		centerPadding: '20px',
+		prevArrow: '<div class="btn-slide slick-prev"><i class="icon-arrow-left"></i></div>',
+		nextArrow: '<div class="btn-slide slick-next"><i class="icon-arrow-right"></i></div>',
+		responsive: [
+		{
+			breakpoint: 959,
+			settings: {
+			centerMode: true,
+			}
+		},
+		{
+			breakpoint: 767,
+			settings: {
+				centerMode: false,
 			}
 		},
 	]
